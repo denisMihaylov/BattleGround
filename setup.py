fe42ad4ed9bb9ec26a5db8a80351680b0705dfdb
Denis Mihaylov <denis.mihaylov93@gmail.com>
2016-05-24 12:07:04 +0300

    Creates setup.py file

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="battle_ground",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Denis Mihaylov",
+    author_email="denis.mihaylov93@gmail.com",
+    description="This is a battle ground that finds the best game bot",
+    license="GNU",
+    keywords="battleground",
+    url="https://github.com/denisMihaylov/BattleGround",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Email',
+        'Topic :: Software Development :: Bug Tracking',
+    ],
+)
