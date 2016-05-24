7d6f656331844670da4ac8035767acb5d1c51d8b
Denis Mihaylov <denis.mihaylov93@gmail.com>
2016-05-24 22:21:46 +0300

    Adds Battle and Bot entity

--- battleground/__init__.py
+++
@@ -1,3 +0,0 @@
-
-class BattleGroundError(Exception):
-	pass
--- battleground/entity/__init__.py
+++ battleground/entity/__init__.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, UnicodeText
+from sqlalchemy import create_engine, UnicodeText, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, VARCHAR, Sequence, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship
@@ -6,6 +6,36 @@ from sqlalchemy.orm import sessionmaker, relationship
 db_engine = create_engine('sqlite:///test.db')
 
 Base = declarative_base()
+
+battle_bots = Table("battle_bots", Base.metadata,\
+    Column('battle_id', ForeignKey('battles.id'), primary_key=True),\
+    Column('bot_id', ForeignKey('bots.id'), primary_key=True))
+
+class Battle(Base):
+    __tablename__ = "battles"
+
+    id = Column(Integer, Sequence('battle_id_seq'), primary_key=True)
+
+    game_id = Column(Integer, ForeignKey('games.id'))
+    game = relationship("Game")
+
+    bots = relationship("Bots", secondary=battle_bots,\
+        back_populates="battles")
+
+class Bot(Base):
+    __tablename__ = "bots"
+
+    id = Column(Integer, Sequence('bot_id_seq'), primary_key=True)
+    rating = Column(Integer)
+
+    game_id = Column(Integer, ForeignKey('games.id'))
+    game = relationship("Game", back_populates="bots")
+
+    author_id = Column(Integer, ForeignKey('users.id'))
+    author = relationship("User", back_populates="bots")
+    
+    battles = relationship("Battle", secondary=battle_bots,\
+        back_populates="bots")
 
 class Game(Base):
     __tablename__ = "games"
@@ -15,6 +45,7 @@ class Game(Base):
     name = Column(String)
     author_id = Column(Integer, ForeignKey('users.id'))
     author = relationship("User", back_populates="games")
+    bots = relationship("Bot", order_by=Bot.rating, back_populates="game")
 
     def __repr__(self):
         return "<Game(source='%s')>" % self.source
@@ -26,6 +57,7 @@ class User(Base):
     username = Column(String)
     password = Column(String)
     games = relationship("Game", order_by=Game.id, back_populates="author")
+    bots = relationship("Bot", order_by=Bot.rating, back_populates="author")
 
     def __eq(self, other):
         return self.username == other.username
@@ -33,7 +65,6 @@ class User(Base):
     def __repr__(self):
         return "<User(id='%s', username='%s', password='%s')>" % \
             (self.id, self.username, self.password)
-
 
 Base.metadata.bind = db_engine
 Base.metadata.create_all()
