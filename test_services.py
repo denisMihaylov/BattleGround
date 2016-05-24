d3bc9410a8858b20669c11bc191510e6213e9579
Denis Mihaylov <denis.mihaylov93@gmail.com>
2016-05-24 22:27:20 +0300

    Generate requirements and fix broken file

--- test_services.py
+++ test_services.py
@@ -2,8 +2,8 @@ import unittest
 from battleground import services
 
 class TestCritic(unittest.TestCase):
-	def test_user_service(self):
-		
-
+    def test_user_service(self):
+        pass
+        
 if __name__ == '__main__':
     unittest.main()
